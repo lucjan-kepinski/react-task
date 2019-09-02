9b7d8682411cb03cab8c7f8ae81d73e90143b49f
Lucjan <lucjan.kepinski@gmail.com>
2019-09-02 11:53:05 +0200

    authenticate added on button click

--- src/App.js
+++ src/App.js
@@ -2,86 +2,7 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Login from "./components/Login"
-
-const REST_API_URL = 'http://frontend-recruitment.one2tribe.pl:8080';
-// const REST_API_URL = 'http://localhost:3000';
-
-const AUTH_OPTIONS = {
-    credentials: 'include',
-    mode: 'cors',
-};
-
-const authenticate = async () => {
-    const body = JSON.stringify({
-        username: 'YNAPEyJk',
-        password: 'ylYJDgFmnAIs'
-    });
-
-    const response = await fetch(`${REST_API_URL}/api/authenticate`, {
-        method: 'post',
-        ...AUTH_OPTIONS,
-        body,
-        headers: {
-            'Content-Type': 'application/json',
-            Accept: 'application/json'
-        }
-    });
-
-    return response.text();
-};
-
-const retrieveItems = async () => {
-    const response = await fetch(`${REST_API_URL}/api/v1/item`, {
-        ...AUTH_OPTIONS,
-        headers: {
-            Authorization: "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJzZWN1cmUtYXBpIiwiYXVkIjoic2VjdXJlLWFwcCIsInN1YiI6IllOQVBFeUprIiwiZXhwIjoxNTY4MjA1NTMwLCJyb2wiOlsiVXNlciJdfQ.2wMBMUUb8FENFj2zOGKCyeYEQLpL1bVthoaGvgTFMYTzl12x9oEApk4hZC88h6Xma-SIC9D5rS09op-7R3RAGQ",
-            'Content-Type': 'application/json',
-            Accept: 'application/json'
-        }
-    });
-
-    return response.json();
-};
-
-const addItem = async (item) => {
-    const response = await fetch(`${REST_API_URL}/api/v1/item`, {
-        method: 'post',
-        ...AUTH_OPTIONS,
-        body: JSON.stringify(item),
-        headers: {
-          Authorization: "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJzZWN1cmUtYXBpIiwiYXVkIjoic2VjdXJlLWFwcCIsInN1YiI6IllOQVBFeUprIiwiZXhwIjoxNTY4MjA1NTMwLCJyb2wiOlsiVXNlciJdfQ.2wMBMUUb8FENFj2zOGKCyeYEQLpL1bVthoaGvgTFMYTzl12x9oEApk4hZC88h6Xma-SIC9D5rS09op-7R3RAGQ",
-            'Content-Type': 'application/json',
-            Accept: 'application/json'
-        }
-    });
-    
-    return response.json();
-};
-
-const main = async () => {
-    await authenticate();
-
-    const items = await retrieveItems();
-
-    console.log(items);
-};
-
-main().catch(error => console.error(error));
-
-const button = document.createElement('button');
-
-button.onclick = () => {
-    console.log('click');
-
-    const name = 'abc';
-    const item = {name};
-
-    addItem(item).catch(error => console.error(error));
-};
-
-button.textContent = 'Dodaj';
-
-// document.body.appendChild(button);
+import { authenticate, retrieveItems, main } from "./services/AuthService"
 
 function App() {
     const [state, setState] = useState({
@@ -95,14 +16,13 @@ function App() {
         isLoggedIn: true})
 
     function onClickSignIn(event, email, password) {
+        authenticate().then(newToken => setState({...state, token: newToken}))
         setState({
             email,
             password,
             isLoggedIn: true})
 
         event.preventDefault()
-
-        let userFound = false
 
                 if (state.email === "YNAPEyJk" && state.password === "ylYJDgFmnAIs") {
                     return alert("sukces")
@@ -125,18 +45,13 @@ function onLoginChange(event){
 function onPasswordChange(event){
     setState({
        ...state,
-       password: event.target.value
+       password: event.target.value,
 })}
 
-  return (
-    state.email === "YNAPEyJk" && state.password === "ylYJDgFmnAIs" ?
+  return ( <>{console.log(state)}
+    {state.isLoggedIn ?
     <div className="App">     
-    <Login 
-    onClick={(event, email, password) => onClickSignIn(event, email, password)} 
-    onLoginChange={(event) => onLoginChange(event)}
-    onPasswordChange={(event) => onPasswordChange(event)}
-    email={state.email} 
-    password={state.password}/>
+        dupa
     </div> :
     <div className="App">     
         <Login 
@@ -145,8 +60,8 @@ function onPasswordChange(event){
         onPasswordChange={(event) => onPasswordChange(event)}
         email={state.email} 
         password={state.password}/>
-    </div>
-  );
+    </div>}
+  </>);
 }
 
 export default App;
---
+++ src/services/AuthService.js
@@ -0,0 +1,85 @@
+const REST_API_URL = "http://frontend-recruitment.one2tribe.pl:8080";
+// const REST_API_URL = 'http://localhost:3000';
+
+const AUTH_OPTIONS = {
+  credentials: "include",
+  mode: "cors"
+};
+
+export const authenticate = async () => {
+  const body = JSON.stringify({
+    username: "YNAPEyJk",
+    password: "ylYJDgFmnAIs"
+  });
+
+  const response = await fetch(`${REST_API_URL}/api/authenticate`, {
+    method: "post",
+    ...AUTH_OPTIONS,
+    body,
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    }
+  });
+  setTimeout(() => {
+    // const newResponse = response.getAllResponseHeaders();
+    console.log(response);
+  }, 3000);
+
+  return response.text();
+};
+
+export const retrieveItems = async () => {
+  const response = await fetch(`${REST_API_URL}/api/v1/item`, {
+    ...AUTH_OPTIONS,
+    headers: {
+      Authorization:
+        "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJzZWN1cmUtYXBpIiwiYXVkIjoic2VjdXJlLWFwcCIsInN1YiI6IllOQVBFeUprIiwiZXhwIjoxNTY4MjA1NTMwLCJyb2wiOlsiVXNlciJdfQ.2wMBMUUb8FENFj2zOGKCyeYEQLpL1bVthoaGvgTFMYTzl12x9oEApk4hZC88h6Xma-SIC9D5rS09op-7R3RAGQ",
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    }
+  });
+
+  return response.json();
+};
+
+export const addItem = async item => {
+  const response = await fetch(`${REST_API_URL}/api/v1/item`, {
+    method: "post",
+    ...AUTH_OPTIONS,
+    body: JSON.stringify(item),
+    headers: {
+      Authorization:
+        "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJzZWN1cmUtYXBpIiwiYXVkIjoic2VjdXJlLWFwcCIsInN1YiI6IllOQVBFeUprIiwiZXhwIjoxNTY4MjA1NTMwLCJyb2wiOlsiVXNlciJdfQ.2wMBMUUb8FENFj2zOGKCyeYEQLpL1bVthoaGvgTFMYTzl12x9oEApk4hZC88h6Xma-SIC9D5rS09op-7R3RAGQ",
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    }
+  });
+
+  return response.json();
+};
+
+export const main = async () => {
+  await authenticate();
+
+  const items = await retrieveItems();
+
+  console.log(items);
+};
+
+// main().catch(error => console.error(error));
+
+const button = document.createElement("button");
+
+button.onclick = () => {
+  console.log("click");
+
+  const name = "abc";
+  const item = { name };
+
+  addItem(item).catch(error => console.error(error));
+};
+
+button.textContent = "Dodaj";
+
+// document.body.appendChild(button);
