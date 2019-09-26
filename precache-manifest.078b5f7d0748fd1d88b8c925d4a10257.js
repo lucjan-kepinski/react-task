8343c4d364a0c12563f49f7a8116a541ed8cacee
Lucjan <lucjan.kepinski@gmail.com>
2019-09-26 14:38:17 +0200

    Updates

---
+++ precache-manifest.078b5f7d0748fd1d88b8c925d4a10257.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11bb4ea84e9141a151b6061543c80b25",
+    "url": "/react-task/index.html"
+  },
+  {
+    "revision": "8ac4ec81c3593cb36985",
+    "url": "/react-task/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "d0cbdde1707983ac0a3f",
+    "url": "/react-task/static/js/2.0b3e55a9.chunk.js"
+  },
+  {
+    "revision": "8ac4ec81c3593cb36985",
+    "url": "/react-task/static/js/main.8129a8a6.chunk.js"
+  },
+  {
+    "revision": "2eab3dbca0e464b853a7",
+    "url": "/react-task/static/js/runtime~main.9f1b832b.js"
+  }
+]);
